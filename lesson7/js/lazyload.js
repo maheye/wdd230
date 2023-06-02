fb0627f930eda7f38f9e538b57276a5c406d3a8d
maheye <u277721157@byui.edu>
2023-06-02 13:43:30 +0800

    added

---
+++ lesson7/js/lazyload.js
@@ -0,0 +1,33 @@
+// Get all images to be replaced
+const imagesToLoad = document.querySelectorAll("[data-src]");
+
+// function to load the actual image
+const loadImages = (img) => {
+  img.setAttribute("src", img.getAttribute("data-src"));
+  img.onload = () => {
+    img.removeAttribute("data-src");
+  };
+};
+
+const imageOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px 50px 0px",
+};
+
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  }, imageOptions);
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
