32cc823a5de833994ab0719d18888d23643365ff
maheye <u277721157@byui.edu>
2023-10-21 19:35:39 +0800

    table

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -24,3 +24,18 @@ modeButton.addEventListener("click", () => {
 		modeButton.textContent = "🕶️";
 	}
 });
+
+//visits
+//visits
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+  visitsDisplay.textContent = numVisits;
+} else {
+  visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
